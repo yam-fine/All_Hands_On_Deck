e07cec3e2ca5f94d13121b92a089e512c87f7ab2
yamdeking <yamf45@gmail.com>
2023-06-18 00:16:34 +0300

    working

--- Assets/Scripts/Climb.cs
+++ Assets/Scripts/Climb.cs
@@ -6,16 +6,58 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Climb : MonoBehaviour
 {
-    CharacterController character;
-    public static XRController climbingHand;
+    Rigidbody rb;
+    LocomotionSystem ls;
+    public static ActionBasedController climbingHand;
+
+    private ActionBasedController previousHand;
+    private Vector3 previousPos;
+    private Vector3 velocity;
 
     private void Start() {
-        character = GetComponent<CharacterController>();
-
+        rb = GetComponent<Rigidbody>();
+        ls = GetComponent<LocomotionSystem>();
     }
 
+    //private void FixedUpdate() {
+    //    if (climbingHand) {
+    //        ls.enabled = false;
+    //        Character_Climb();
+    //    }
+    //    else {
+    //        ls.enabled = true;
+    //    }
+    //}
+
+
+    void Update() {
+
+        if (climbingHand) {
+            if (previousHand == null) {
+                previousHand = climbingHand;
+                previousPos = climbingHand.positionAction.action.ReadValue<Vector3>();
+            }
+            if (climbingHand.name != previousHand.name) {
+                previousHand = climbingHand;
+                previousPos = climbingHand.positionAction.action.ReadValue<Vector3>();
+                //Debug.Log("DIFFERENT HAND NOW");
+            }
+            ls.enabled = false;
+            Character_Climb();
+        }
+        else {
+            ls.enabled = true;
+        }
+    }
+
+
     private void Character_Climb() {
-        InputDevices.GetDeviceAtXRNode(climbingHand.controllerNode).TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
-        character.Move(transform.rotation * -velocity * Time.fixedDeltaTime);
+        Debug.Log("climb climb");
+        //InputDevices.GetDeviceAtXRNode(climbingHand.).TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
+        //Debug.Log(velocity);
+        var velocity = (climbingHand.positionAction.action.ReadValue<Vector3>() - previousPos) / Time.deltaTime;
+        Debug.Log(velocity);
+        rb.AddForce(transform.rotation * -velocity * Time.fixedDeltaTime * 500);
+        previousPos = climbingHand.positionAction.action.ReadValue<Vector3>();
     }
 }
--- Assets/Scripts/ClimbInteractor.cs
+++ Assets/Scripts/ClimbInteractor.cs
@@ -6,21 +6,28 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ClimbInteractor : XRBaseInteractable
 {
     [System.Obsolete]
-    protected override void OnSelectEntered(XRBaseInteractor interactor) {
-        base.OnSelectEntered(interactor);
-
-        if (interactor is XRDirectInteractor)
-            Climb.climbingHand = interactor.GetComponent<XRController>();
+    protected override void OnSelectEntered(SelectEnterEventArgs args) {
+        XRBaseInteractor interactor = args.interactor;
+        base.OnSelectEntered(args);
+        if (interactor is XRDirectInteractor) {
+            Debug.Log("HELLO");
+            Climb.climbingHand = interactor.GetComponent<ActionBasedController>();
+            Debug.Log(Climb.climbingHand);
+        }
     }
 
-    [System.Obsolete]
-    protected override void OnSelectExited(XRBaseInteractor interactor) {
-        base.OnSelectEntered(interactor);
+    protected override void OnSelectExited(SelectExitEventArgs args) {
+        base.OnSelectExited(args);
 
-        if (interactor is XRDirectInteractor) {
-            if (Climb.climbingHand && Climb.climbingHand.name == interactor.name) {
-                Climb.climbingHand = null;
-            }
+        //if (interactor is XRDirectInteractor) {
+        //    if (Climb.climbingHand && Climb.climbingHand.name == interactor.name) {
+        //        Climb.climbingHand = null;
+        //    }
+        //}
+
+        if (Climb.climbingHand && Climb.climbingHand.name == args.interactorObject.transform.name) {
+            Climb.climbingHand = null;
         }
+
     }
 }
