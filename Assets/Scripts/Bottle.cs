23e08c296d50acc2ca1a4c934b538e5ef0a14b7f
Artume Pichersky <artume99@gmail.com>
2023-06-18 11:26:48 +0300

    bottle spill

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Bottle : MonoBehaviour
+{
+    private bool _open;
+    [SerializeField] private XRGrabInteractable cap;
+
+    void Start()
+    {
+        cap.enabled = false;
+    }
+
+    void Update()
+    {
+
+    }
+    public bool IsOpen()
+    {
+        return _open;
+    }
+
+    public void Open()
+    {
+        _open = true;
+    }
+
+    public void Close()
+    {
+        _open = false;
+    }
+
+    public void OnBottleHold(SelectEnterEventArgs args)
+    {
+        if (IsOpen())
+            return;
+        cap.enabled = true;
+    }
+
+    public void OnBottleRelease(SelectExitEventArgs args)
+    {
+        if (IsOpen())
+            return;
+        cap.enabled = false;
+    }
+
+    public void OnBottleOpen(SelectExitEventArgs args)
+    {
+        Debug.Log("Open");
+
+        Open();
+    }
+    public void OnBottleClose(SelectEnterEventArgs args)
+    {
+        //Debug.Log("Close");
+        //Close();
+    }
+}
