920754814fd318a2423303bf88d92ff06b104c91
Artume Pichersky <artume99@gmail.com>
2023-07-15 13:41:55 +0300

    ב

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -10,13 +10,7 @@ public class Bottle : MonoBehaviour
 
     void Start()
     {
-<<<<<<< Updated upstream
-        cap.enabled = false;
-=======
-        capInteractable = cap.GetComponent<XRGrabInteractable>();
-        capInteractable.enabled = true;
-        cap.GetComponent<Rigidbody>().isKinematic = true;
->>>>>>> Stashed changes
+
     }
 
     void Update()
@@ -42,22 +36,14 @@ public class Bottle : MonoBehaviour
     {
         if (IsOpen())
             return;
-<<<<<<< Updated upstream
-        cap.enabled = true;
-=======
         //capInteractable.enabled = true;
->>>>>>> Stashed changes
     }
 
     public void OnBottleRelease(SelectExitEventArgs args)
     {
         if (IsOpen())
             return;
-<<<<<<< Updated upstream
-        cap.enabled = false;
-=======
         //capInteractable.enabled = false;
->>>>>>> Stashed changes
     }
 
     public void OnBottleOpen(SelectExitEventArgs args)
@@ -65,22 +51,12 @@ public class Bottle : MonoBehaviour
         Debug.Log("Open");
 
         Open();
-<<<<<<< Updated upstream
     }
     public void OnBottleClose(SelectEnterEventArgs args)
     {
         //Debug.Log("Close");
         //Close();
-=======
         cap.GetComponent<Rigidbody>().isKinematic = false;
         //cap.transform.parent = null;
-    }
-    public void OnBottleClose(SelectEnterEventArgs args)
-    {
-        Debug.Log("Close");
-        Close();
-        cap.GetComponent<Rigidbody>().isKinematic = true;
-        //cap.transform.parent = transform;
->>>>>>> Stashed changes
     }
 }
