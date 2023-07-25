9f4aa35f21c8fe5128b282fc6415e41155b841b9
yamdeking <yamf45@gmail.com>
2023-07-25 20:07:37 +0300

    state controller and audio

---
+++ Assets/Scripts/StateMachine/Room1Day1/WaterPickup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPickup : IsState
+{
+    public void OnEnter(StateController sc) {
+        // character sits on bed
+
+        //temp
+        sc.ChangeState(sc.room1Roam);
+    }
+
+    public void OnExit(StateController sc) {
+        // allow character to walk
+    }
+
+    public void UpdateState(StateController sc) {
+        // if drank water successfully
+        // change state
+    }
+}
