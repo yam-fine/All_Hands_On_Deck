515ca3b3de898a6027810ab4eef03519d6597d26
Nitay Shoshana <nitay2020@gmail.com>
2023-07-27 10:38:42 +0300

    WIP: hand animations

---
+++ Assets/Scripts/Player/HandAnimationManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimationManager : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private InputActionReference activateReference;
+
+    [SerializeField]
+    private InputActionReference selectReference;
+
+    private InputAction activateAction;
+    private InputAction selectAction;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        activateAction = activateReference.action;
+        selectAction = selectReference.action;
+
+        // if (animator != null && animator.isActiveAndEnabled)
+        // {
+        //     animator.Play("Blend Tree");
+        // }
+    }
+
+
+    private void Update() {
+        Debug.Log("READING: " + activateAction.ReadValue<float>());
+        animator.SetFloat("Activate", activateAction.ReadValue<float>());
+        animator.SetFloat("Select", selectAction.ReadValue<float>());
+    }
+
+}
