ca7a9ab7818e953f95a960d17f652993047fc697
yamdeking <yamf45@gmail.com>
2023-08-01 14:46:52 +0300

    bottle drink

---
+++ Assets/Scripts/DetectWaterDrink.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectWaterDrink : MonoBehaviour
+{
+    private void OnParticleCollision(GameObject other) {
+        Debug.Log("bloop");
+    }
+}
