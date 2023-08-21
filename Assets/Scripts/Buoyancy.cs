9128cc979c6cab9eda120e8464919aad044eed20
yamdeking <yamf45@gmail.com>
2023-08-21 20:32:18 +0300

    wheel turns ship

--- Assets/Scripts/Buoyancy.cs
+++ Assets/Scripts/Buoyancy.cs
@@ -3,23 +3,31 @@ using UnityEngine;
 public class Buoyancy : MonoBehaviour {
     [SerializeField] float swayAmount = 1f; // Parameter to control the amount of sway
     [SerializeField] float swayFrequency = 1f; // Parameter to control the frequency of the sway
+    [SerializeField] Transform wheel;
+    [SerializeField] float rotationSpeed = 100;
 
-    private float originalY;
     private float timeCounter;
+    float rotateDir = 0;
 
     private void Start() {
-        originalY = transform.position.y;
         timeCounter = 0f;
     }
 
-    private void Update() {
+    private void Update() {    
+        if (wheel.rotation.x < 0)
+            rotateDir += -1;
+        else if (wheel.rotation.x > 0)
+            rotateDir += 1;
+
         // Calculate the sway angle using a sine wave based on time
         float swayAngle = Mathf.Sin(timeCounter * swayFrequency) * swayAmount;
 
         // Rotate the ship around the up vector (y-axis) to simulate sway
-        transform.rotation = Quaternion.Euler(0f, 0f, swayAngle);
+        transform.rotation = Quaternion.Euler(0f, rotateDir, swayAngle);
 
         // Increment the time counter based on time to control the speed of sway
         timeCounter += Time.deltaTime;
+
+        //transform.Rotate(Vector3.up, 1000 * Time.deltaTime);
     }
 }
