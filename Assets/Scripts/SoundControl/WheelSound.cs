2a787485d9b58e28a581191f3ba3b9aaf210ae7b
nitay <nitay@careboxhealth.com>
2023-08-29 20:22:58 +0300

    add: stump bandage (still needs material)

---
+++ Assets/Scripts/SoundControl/WheelSound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class WheelSound : MonoBehaviour
+{
+
+    public AudioManager audioManager;
+
+    void Start() {
+    }
+
+    void Update() {
+        
+        
+    }
+
+    public void WheelMove() {
+        //audioManager.PlaySound(AudioManager.Sounds.wheelStir);
+    }
+
+    
+    
+}
