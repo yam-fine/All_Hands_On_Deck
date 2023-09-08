acb2c1676517f66d8a27f8dc45120ce34072ce8d
yamdeking <yamf45@gmail.com>
2023-09-08 17:23:27 +0300

    up to tele under ladder

--- Assets/Scripts/StateMachine/ShipDay2/Final.cs
+++ Assets/Scripts/StateMachine/ShipDay2/Final.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Final : IsState {
+
+    float timer = 0;
     public void OnEnter(StateController sc) {
         InnerOnEnter((ShipDay2)sc);
     }
@@ -20,13 +22,10 @@ public class Final : IsState {
     }
 
     private void InnerUpdate(ShipDay2 sc) {
-        float timer = 0;
-        Debug.Log("hi");
         while (timer < sc.fade.fadeDur) {
             timer += Time.deltaTime;
             return;
         }
-        Debug.Log("hiii");
         sc.player.transform.position = sc.teleportAfterLadder.position;
         sc.player.transform.rotation = sc.teleportAfterLadder.rotation;
         sc.fade.FadeIn();
